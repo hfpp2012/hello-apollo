efa8a5f1eefd1ace0c61bc8f46e956150a9406e5
hfpp2012 <hfpp2012@gmail.com>
2019-07-02 16:08:17 +0800

    06_apollo_client

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './App.css';
 import logo from './logo.png';
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 function App() {
   return (
-    <div className="App">
-      <img
-        src={logo}
-        alt="Spacex"
-        style={{ width: 300, display: "block", margin: "auto" }}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <img
+          src={logo}
+          alt="Spacex"
+          style={{ width: 300, display: "block", margin: "auto" }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
